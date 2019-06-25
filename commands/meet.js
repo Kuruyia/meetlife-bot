dd235137a250db90610d920a4aada7648461764c
Kuruyia <alexandresollier@gmail.com>
2019-06-26 00:18:23 +0200

    Added search commands to the meeting manager

--- commands/meet.js
+++ commands/meet.js
@@ -71,16 +71,99 @@ module.exports = {
                 stuff.sendUsage(stuff.message.channel, this.name + ' add', '[name] [location] [start _(and end)_ date]');
             }
         } else if (stuff.args[0] == 'list') {
-            stuff.dbObjects.UpcomingMeetings.findAll({
-                limit: 10,
-                order: [[stuff.dbObjects.sequelize.col('start_time'), stuff.args[1] == 'desc' ? 'DESC' : 'ASC']]
-            }).then(upcomingMeetings => {
-                for (i = 0; i < upcomingMeetings.length; i++) {
-                    stuff.message.reply(upcomingMeetings[i].dataValues.id);
+            if (!stuff.args[1] || stuff.args[1] == 'asc' || stuff.args[1] == 'desc') {
+                stuff.dbObjects.UpcomingMeetings.findAll({
+                    limit: 10,
+                    order: [[stuff.dbObjects.sequelize.col('start_time'), stuff.args[1] == 'desc' ? 'DESC' : 'ASC']]
+                }).then(upcomingMeetings => {
+                    module.exports.sendSearchResult(stuff, upcomingMeetings, 'Upcoming meeting list', stuff.args[1] == 'desc' ? 'Sorted from descending order' : 'Sorted from ascending order');
+                });
+            } else {
+                stuff.sendUsage(stuff.message.channel, this.name + ' list', '_(asc/desc)_');
+            }
+        } else if (stuff.args[0] == 'search') {
+            if (stuff.args.length >= 2 && (stuff.args[1] == 'location' || stuff.args[1] == 'day' || stuff.args[1] == 'owner' || stuff.args[1] == 'name')) {
+                if (stuff.args[1] == 'location') {
+                    if (stuff.args.length >= 3) {
+                        stuff.dbObjects.UpcomingMeetings.findAll({
+                            limit: 10,
+                            where: {
+                                location_name: {
+                                    [stuff.dbObjects.seqOp.substring]: stuff.args[2]
+                                }
+                            }
+                        }).then(searchResults => {
+                            module.exports.sendSearchResult(stuff, searchResults, 'Search by location - "' + stuff.args[2] + '"');
+                        });
+                    } else {
+                        stuff.sendUsage(stuff.message.channel, this.name + ' search location', '[location]');
+                    }
+                } else if (stuff.args[1] == 'day') {
+                    if (stuff.args.length >= 3) {
+                        const chronoRes = stuff.chronode.parse(stuff.args[2]);
+
+                        if (chronoRes.length == 0) {
+                            stuff.sendError(stuff.message.channel, 'Unable to find a valid date in "' + stuff.args[2] + '"');
+                            return;
+                        }
+
+                        const queryDate = chronoRes[0].start.date();
+                        queryDate.setHours(0);
+                        queryDate.setMinutes(0);
+                        queryDate.setSeconds(0);
+
+                        const startTimestamp = queryDate.getTime() / 1000;
+                        const endTimestamp = startTimestamp + 86400;
+
+                        stuff.dbObjects.UpcomingMeetings.findAll({
+                            limit: 10,
+                            where: {
+                                start_time: {
+                                    [stuff.dbObjects.seqOp.between]: [startTimestamp, endTimestamp]
+                                }
+                            }
+                        }).then(searchResults => {
+                            module.exports.sendSearchResult(stuff, searchResults, 'Search by day - ' + queryDate.toLocaleDateString('en-GB'));
+                        });
+                    } else {
+                        stuff.sendUsage(stuff.message.channel, this.name + ' search day', '[date]');
+                    }
+                } else if (stuff.args[1] == 'owner') {
+                    if (stuff.args.length >= 3) {
+                        stuff.dbObjects.UpcomingMeetings.findAll({
+                            limit: 10,
+                            where: {
+                                owner_id: {
+                                    [stuff.dbObjects.seqOp.eq]: stuff.message.mentions.members.first().id
+                                }
+                            }
+                        }).then(searchResults => {
+                            module.exports.sendSearchResult(stuff, searchResults, 'Search by owner - @' + stuff.message.mentions.members.first().user.tag);
+                        });
+                    } else {
+                        stuff.sendUsage(stuff.message.channel, this.name + ' search owner', '[owner mention]');
+                    }
+                } else if (stuff.args[1] == 'name') {
+                    if (stuff.args.length >= 3) {
+                        stuff.dbObjects.UpcomingMeetings.findAll({
+                            limit: 10,
+                            where: {
+                                name: {
+                                    [stuff.dbObjects.seqOp.substring]: stuff.args[2]
+                                }
+                            }
+                        }).then(searchResults => {
+                            module.exports.sendSearchResult(stuff, searchResults, 'Search by name - "' + stuff.args[2] + '"');
+                        });
+                    } else {
+                        stuff.sendUsage(stuff.message.channel, this.name + ' search name', '[meeting name]');
+                    }
                 }
-            });
+            } else {
+                stuff.sendUsage(stuff.message.channel, this.name + ' search', '[location/day/owner/name]');
+            }
         } else if (stuff.args.length == 0) {
-            stuff.sendUsage(stuff.message.channel, this.name, ['add [name] [location] [start _(and end)_ date]', 'list _(asc/desc)_']);
+            stuff.sendUsage(stuff.message.channel, this.name, ['add [name] [location] [start _(and end)_ date]', 'list _(asc/desc)_', 'search [location/day/owner/name]']);
         } else {
             stuff.sendError(stuff.message.channel, 'Unknown option: **' + stuff.args[0] + '**');
         }
@@ -104,4 +187,30 @@ module.exports = {
         
         stuff.message.reply('ok');
     },
+
+    sendSearchResult: function(stuff, result, title, footer) {
+        const constructedEmbed = new stuff.discord.RichEmbed();
+        
+        if (result.length > 0) {
+            constructedEmbed.setColor('BLUE');
+
+            var message = '';
+            for (i = 0; i < result.length; i++) {
+                const currentMeetingData = result[i].dataValues;
+                message = message.concat('**#' + currentMeetingData.id + '**: ' + currentMeetingData.name + ' - ' + new Date(currentMeetingData.start_time * 1000).toLocaleString('en-GB') + '\n');
+            }
+
+            constructedEmbed.addField(title, message);
+            if (footer) {
+                constructedEmbed.setFooter(footer + ' - ' + result.length + (result.length > 1 ? ' results' : ' result'));
+            } else {
+                constructedEmbed.setFooter(result.length + (result.length > 1 ? ' results' : ' result'))
+            }
+        } else {
+            constructedEmbed.setColor('RED');
+            constructedEmbed.addField(title, 'No result found');
+        }
+
+        stuff.message.channel.send(constructedEmbed);
+    }
 };
--- dbObjects.js
+++ dbObjects.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 
+const seqOp = Sequelize.Op;
 const sequelize = new Sequelize('database', 'username', 'password', {
 	host: 'localhost',
 	dialect: 'sqlite',
@@ -11,4 +12,4 @@ const UpcomingMeetings = sequelize.import('models/UpcomingMeetings');
 
 sequelize.sync();
 
-module.exports = {sequelize, UpcomingMeetings};
+module.exports = {sequelize, seqOp, UpcomingMeetings};
