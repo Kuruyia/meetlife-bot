5aa64d5b4ab433410a02bf59a60515ffa59bce19
Kuruyia <alexandresollier@gmail.com>
2019-06-25 18:47:46 +0200

    Updated UpcomingMeetings model

--- models/UpcomingMeetings.js
+++ models/UpcomingMeetings.js
@@ -1,10 +1,13 @@
 module.exports = (sequelize, DataTypes) => {
 	return sequelize.define('upcoming_meeting', {
 		name: DataTypes.STRING,
-		time: {
+		start_time: {
 			type: DataTypes.DATE,
 			allowNull: false,
 		},
+		end_time: DataTypes.DATE,
+		longitude: DataTypes.REAL,
+		latitude: DataTypes.REAL,
 	}, {
 		timestamps: false,
 	});
