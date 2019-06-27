709abd6970db1e787954ba492ba0b344e932eb2c
Kuruyia <alexandresollier@gmail.com>
2019-06-27 23:25:50 +0200

    Remove test option in choice command

--- commands/choice.js
+++ commands/choice.js
@@ -3,21 +3,7 @@ module.exports = {
     description: 'Choice manager',
     
 	execute(stuff) {
-        if (stuff.args[0] == 'test') {
-            tst = [];
-            for (i = 1; i < stuff.args.length; i++) {
-                tst.push(stuff.args[i]);
-            }
-
-            const rep = stuff.choiceMan.addChoice(stuff.message.author.id, new stuff.choice('Test choice', tst, tst, function(option, data) {
-                stuff.message.reply(data[option]);
-            }));
-            if (rep != true) {
-                stuff.sendUtils.sendError(stuff.message.channel, stuff.message.author.id, rep);
-            }
-
-            console.log(tst);
-        } else if (stuff.args[0] == 'list') {
+        if (stuff.args[0] == 'list') {
             const rep = stuff.choiceMan.sendChoicesToChannel(stuff, stuff.message.author.id, stuff.message.channel, stuff.config.prefix, stuff.message.author.id);
             if (rep != true) {
                 stuff.sendUtils.sendError(stuff.message.channel, stuff.message.author.id, rep);
