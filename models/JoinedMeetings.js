44d2bee5dcae4710ff547256d7734f2ee80aa0af
Kuruyia <alexandresollier@gmail.com>
2019-06-27 09:46:49 +0200

    Update user_id type to fix integer limit

--- models/JoinedMeetings.js
+++ models/JoinedMeetings.js
@@ -1,7 +1,7 @@
 module.exports = (sequelize, DataTypes) => {
 	return sequelize.define('joined_meetings', {
 		user_id: {
-			type: DataTypes.INTEGER,
+			type: DataTypes.STRING,
 			allowNull: false,
 			unique: 'uniqueJoin'
 		},
